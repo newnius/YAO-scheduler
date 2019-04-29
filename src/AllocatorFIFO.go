778f3e4c1f7f29c973c165f32c934b867e8e2db4
Newnius <newnius.cn@gmail.com>
2019-04-29 20:29:56 +0800

    update

--- src/AllocatorFIFO.go
+++ src/AllocatorFIFO.go
@@ -3,7 +3,8 @@ package main
 import (
 	"sync"
 	"time"
-	)
+	"fmt"
+)
 
 type AllocatorFIFO struct {
 	history    []*Job
@@ -98,6 +99,7 @@ func (allocator *AllocatorFIFO) requestResource(task Task) NodeStatus {
 				for j := range node.Status {
 					if res.Status[i].UUID == node.Status[j].UUID {
 						node.Status[j].MemoryAllocated = task.MemoryGPU
+						fmt.Println(node.Status[j].UUID)
 					}
 				}
 			}
