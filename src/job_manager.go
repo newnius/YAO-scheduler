5ad6eed3a156f4bf6dafd229b5ea270263893d99
Newnius <newnius.cn@gmail.com>
2020-05-04 20:08:33 +0800

    update

--- src/job_manager.go
+++ src/job_manager.go
@@ -259,6 +259,7 @@ func (jm *JobManager) status() MsgJobStatus {
 	}
 
 	for i, task := range jm.job.Tasks {
+		log.Info(i, jm.job.Tasks)
 		taskStatus := jm.jobStatus.tasks[task.Name]
 		spider := Spider{}
 		spider.Method = "GET"
